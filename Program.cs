d4d73e602a5340258917473070e85a0144b0dd1e
Rosyazh <rossol.zheka@gmail.com>
2016-08-11 16:48:10 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -95,61 +95,92 @@ namespace TenthTask
         {
             return GetEnumerator();
         }
+
+        public void Sort()
+        {
+            contents.Sort();
+        }
     }
 
-    class Worker
+    class Worker: IComparable<Worker>
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string Position { get; set; }
+        public Position WorkPosition { get; set; }
 
         public override string ToString()
         {
-            return string.Format("{0} {1} {2}", Name, Surname, Position);
+            return string.Format("{0} {1} {2}", Name, Surname, WorkPosition);
+        }
+
+        public int CompareTo(Worker other)
+        {
+            if (Equals(other)) return 0;
+            return WorkPosition.CompareTo(other.WorkPosition);
         }
     }
-    
+
+    enum Position
+    {
+        Director = 1,
+        Manager = 2,
+        Bookkeeper = 3
+    }
+
     class PositionComparer : EqualityComparer<Worker>
     {
         public override bool Equals(Worker x, Worker y)
         {
-            return x.Position == y.Position; 
+            return x.WorkPosition == y.WorkPosition;
         }
         public override int GetHashCode(Worker obj)
         {
-            return (obj.Position).GetHashCode();
+            return (obj.WorkPosition).GetHashCode();
         }
     }
-    
+
     class Program
     {
         static void Main(string[] args)
         {      
-            Crew cr = new Crew();
-            cr.Add(new Worker { Name = "Petr", Surname = "Ivanov", Position = "Director" });
-            cr.Add(new Worker { Name = "Vika", Surname = "Klichko", Position = "Manager" });
-            cr.Add(new Worker { Name = "Kolya", Surname = "Vikulov", Position = "Bookkeeper" });
+            Crew crew = new Crew();
+            crew.Add(new Worker { Name = "Petr", Surname = "Ivanov", WorkPosition = Position.Director });
+            crew.Add(new Worker { Name = "Vika", Surname = "Klichko", WorkPosition = Position.Bookkeeper });
+            crew.Add(new Worker { Name = "Kolya", Surname = "Vikulov", WorkPosition = Position.Manager });
+            
+            //sort by LINQ
+            var team = crew.OrderBy(n => n.WorkPosition);
+            Console.WriteLine("Sort with LINQ:\n");
+            foreach(var worker in team)
+                Console.WriteLine(worker);
+
+            //sort by IComparable
+            crew.Sort();
+            Console.WriteLine("\nSort with IComparable:\n");
+            foreach(var worker in crew)
+                Console.WriteLine(worker);
+            
             /*
             Dictionary<int, string> pos = new Dictionary<int, string>();
             pos[1] = "Director";
             pos[2] = "Manager";
             pos[3] = "Bookkeeper";
             */
-            var eqComparer = new PositionComparer();
+            /*var eqComparer = new PositionComparer();
             var d = new Dictionary<Worker, string>(eqComparer);
-            d[cr[0]] = "Director";
-            d[cr[1]] = "Manager";
-            d[cr[2]] = "Bookkeeper";
+            d[crew[0]] = "Director";
+            d[crew[1]] = "Manager";
+            d[crew[2]] = "Bookkeeper";
 
             foreach (Worker c in d.Keys)
             {
                 Console.WriteLine(c.ToString());
-            }
+            }*/
 
             /*
-            foreach (Worker rab in cr)
+            foreach (Worker rab in crew)
             {
-                Console.WriteLine("{0} {1} {2}", rab.Name.PadRight(15), rab.Surname.PadRight(15), rab.Position.ToString().PadRight(10));
+                Console.WriteLine("{0} {1} {2}", rab.Name.PadRight(15), rab.Surname.PadRight(15), rab.WorkPosition.ToString().PadRight(10));
             }*/
         }
     }
